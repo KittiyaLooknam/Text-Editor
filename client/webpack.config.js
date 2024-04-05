a7cd7451ee8456843fb3733bd24ac05c8eb22fa0
KittiyaLooknam <kittiya.goldberg1@gmail.com>
2024-04-04 22:48:50 -0400

    add the files and work on the client js files

---
+++ client/webpack.config.js
@@ -0,0 +1,81 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      // Plugin to generate HTML files
+      new HtmlWebpackPlugin({
+        title: 'Jate App',
+        template: './src/index.html',
+        filename: './index.html',
+        chunks: ['main']
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Jate text',
+        template: 'src/install.html',
+        filename: 'install.html',
+        chunks: ['install'],
+      }),
+
+      // Plugin to inject service worker into the build
+      new InjectManifest({
+        swSrc: './src/sw.js', // Path to your service worker file
+        swDest: 'service-worker.js', // Output service worker file name
+      }),
+
+      // Manifest file creation
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE text editor",
+        short_name: "JATE",
+        description: "Just another text editor.",
+        background_color: "#228B22",
+        theme_color: "#228B22",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve(`src/images/logo.png`),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }
+        ]
+      })
+    ],
+
+    // Module rules
+    module: {
+      // Rule for handling CSS files
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Rule for transpiling JavaScript files using Babel
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    }
+  };
+};
